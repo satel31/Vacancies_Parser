7162ec1ddc9ae40fd595eb1b61b5b1e7c7d0ff06
satel31 <blokhnina.tatiana@yandex.ru>
2023-04-04 19:45:49 +0300

    Added some drafts for classes of vacancies

--- src/classes.py
+++ src/classes.py
@@ -1,21 +1,81 @@
-from ABC import ABC, abstractmethod
+from abc import ABC, abstractmethod
 import requests
+
 
 class Vacancy(ABC):
     """Abstract class for vacancies from different platforms"""
 
-    @abstractmethod
-    def get_vacancy_data(self):
+    # @abstractmethod
+    # def sort_vacancies(self):
+    # pass
+
+
+class HHVacancy(Vacancy):
+    """Class vor vacancies from hh.ru"""
+    url = "https://api.hh.ru/vacancies"
+    hh_vacancies = []
+    AMOUNT_OF_VACANCIES = 5
+
+    def __init__(self, key_word) -> None:
+        """Initialize the class HHVacancy and make a dictionary with vacancies data"""
+        self.key_word = key_word
+        data: dict = self.get_vacancy_data()['items']
+        for item in data:
+            print(item)
+            print()
+            self.__name: str = item['name']
+            self.__url: str = item['url']
+            self.__salary_from: int = self.set_salary(item, 'from')
+            self.__salary_to: int = self.set_salary(item, 'to')
+            self.__salary_currency: str | None = self.set_currency(item)
+            self.__salary_for_sort = self.key_for_sorting
+            self.__short_description: str = item['snippet']['responsibility']
+            self.__company_name: str = item['employer']['name']
+            #записывать не в словарь, а в файл!!!
+            self.hh_vacancies.append(self)
+            print(self.hh_vacancies)
+            print()
+            print()
+
+    def set_salary(self, vacancy, key):
+        try:
+            if isinstance(vacancy['salary'][key], int):
+                return vacancy['salary'][key]
+            else:
+                return int(vacancy['salary'][key])
+        except KeyError:
+            return 0
+        except TypeError:
+            return 0
+
+    def set_currency(self, vacancy):
+        try:
+            return vacancy['salary']['currency']
+        except KeyError:
+            return None
+        except TypeError:
+            return None
+
+    @property
+    def key_for_sorting(self):
+        if self.__salary_to >= self.__salary_from:
+            return self.__salary_to
+        else:
+            return self.__salary_from
+
+    def sort_bigger_to_lower(self):
+        self.hh_vacancies.sort(key=self.salary_to)
+
         pass
 
-    @abstractmethod
-    def sort_vacancies(self):
+    def __str__(self):
         pass
-class HHVacancy(Vacancy):
-    pass
+
+    def __repr__(self):
+        pass
+
 
 class SJVacancy(Vacancy):
     pass
 
-hh_test = HHVacancy()
-hh_test.get_vacancy_data()
+
