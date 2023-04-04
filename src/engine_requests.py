970b21e42d6a2727dfcf854f81fb358752318296
satel31 <blokhnina.tatiana@yandex.ru>
2023-04-04 14:21:34 +0300

    Created class SJRequest (gets data from superjob.ru and returns page by page)

--- src/engine_requests.py
+++ src/engine_requests.py
@@ -17,20 +17,20 @@ class EngingeRequest(ABC):
 class HHRequest(EngingeRequest):
     """Class for getting data from hh.ru"""
 
-    def __init__(self, key_word, per_page=10) -> None:
+    def __init__(self, key_word: str, per_page: int = 100) -> None:
         """Initialize the request with parameters of request and url"""
-        self.key_word = key_word
-        self.per_page = per_page
-        self.url = "https://api.hh.ru/vacancies"
+        self.key_word: str = key_word
+        self.per_page: int = per_page
+        self.__url: str = "https://api.hh.ru/vacancies"
 
-    def request_data(self, page=0) -> dict:
+    def request_data(self, page: int = 0) -> dict:
         """Got data from hh.ru"""
-        params = {
+        params: dict = {
             "text": self.key_word,
             "page": page,
             "per_page": self.per_page
         }
-        response = requests.get(self.url, params=params)
+        response = requests.get(self.__url, params=params)
         if response.status_code == 200:
             vacancies = response.json()
             return vacancies
@@ -42,17 +42,40 @@ class HHRequest(EngingeRequest):
         data = self.request_data()
         pages = data['pages']
         for p in range(pages):
-            #pass thorough some other class?
+            # pass thorough some other class?
             return self.request_data(p)
 
 
-
 class SJRequest(EngingeRequest):
-    def __init__(self):
-        pass
+    """Class for getting data from superjob.ru"""
 
-    def request_data(self):
-        pass
+    def __init__(self, key_word, per_page=100) -> None:
+        """Initialize the request with parameters of request and url"""
+        self.key_word = key_word
+        self.per_page = per_page
+        self.url = "https://api.superjob.ru/2.0/vacancies/"
+        self.__id = "v3.r.130655195.6dd0db9873b05e3698bd20bcb8beeaedcd44e706.d98c5a5d24022a2455003edce45e22f69469b9e3"
 
-    def pass_by_page(self):
-        pass
+    def request_data(self, page=0) -> dict:
+        """Got data from superjob.ru"""
+        secret_key = {'X-Api-App-Id': self.__id}
+        params = {
+            "text": self.key_word,
+            "page": page,
+            "count": self.per_page
+        }
+        response = requests.get(self.url, headers=secret_key, params=params)
+        if response.status_code == 200:
+            vacancies = response.json()
+            return vacancies
+        else:
+            print("Error:", response.status_code)
+
+    def pass_by_page(self) -> dict:
+        """Pass data page by page"""
+        # Максимальное количество сущностей, выдаваемых API равно 500.
+        # Это значит, например, при поиске резюме по 100 резюме на страницу, всего можно просмотреть 5 страниц.
+        pages = int(500 / self.per_page)
+        for p in range(pages):
+            # pass thorough some other class?
+            return self.request_data(p)
